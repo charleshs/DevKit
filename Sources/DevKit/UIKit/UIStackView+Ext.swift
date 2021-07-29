cb2fd6adf23cde0a134519700a6af6f00828e4e0
Charles Hsieh <charlous167@gmail.com>
2021-07-29 09:34:41 +0800

    Add more UIStackView methods

--- Sources/DevKit/UIKit/UIStackView+Ext.swift
+++ Sources/DevKit/UIKit/UIStackView+Ext.swift
@@ -21,4 +21,77 @@ extension UIStackView {
     public func clearArrangedSubviews() {
         arrangedSubviews.forEach(removeArrangedSubview)
     }
+
+    @available(iOS 11.0, tvOS 11.0, *)
+    public func addArrangedSubview(_ subview: UIView, spacingPrev: CGFloat? = nil, spacingNext: CGFloat? = nil) {
+        if let previousView = arrangedSubviews.last, let spacingPrev = spacingPrev {
+            setCustomSpacing(spacingPrev, after: previousView)
+        }
+        addArrangedSubview(subview)
+        spacingNext.map { spacing in
+            setCustomSpacing(spacing, after: subview)
+        }
+    }
+
+    @available(iOS 11.0, tvOS 11.0, *)
+    public func insertArrangedSubview(_ subview: UIView, at index: Int, spacingPrev: CGFloat? = nil, spacingNext: CGFloat? = nil) {
+        insertArrangedSubview(subview, at: index)
+        if index > 0, let spacingPrev = spacingPrev {
+            setCustomSpacing(spacingPrev, after: arrangedSubviews[index - 1])
+        }
+        spacingNext.map { spacing in
+            setCustomSpacing(spacing, after: subview)
+        }
+    }
+
+    @available(iOS 11.0, tvOS 11.0, *)
+    public func insertArrangedSubview(_ subview: UIView, before existingView: UIView, spacingPrev: CGFloat?, spacingNext: CGFloat?) {
+        guard let index = getInsertionPosition(insertingView: subview, exisitingView: existingView) else { return }
+
+        insertArrangedSubview(subview, at: index)
+        spacingNext.map { spacing in
+            setCustomSpacing(spacing, after: subview)
+        }
+        if index > 0, let spacingPrev = spacingPrev {
+            setCustomSpacing(spacingPrev, after: arrangedSubviews[index - 1])
+        }
+    }
+
+    @available(iOS 11.0, tvOS 11.0, *)
+    public func insertArrangedSubview(_ subview: UIView, after existingView: UIView, spacingPrev: CGFloat?, spacingNext: CGFloat?) {
+        guard let index = getInsertionPosition(insertingView: subview, exisitingView: existingView) else { return }
+
+        insertArrangedSubview(subview, at: index.advanced(by: 1))
+        spacingPrev.map { spacing in
+            setCustomSpacing(spacing, after: existingView)
+        }
+        spacingNext.map { spacing in
+            setCustomSpacing(spacing, after: subview)
+        }
+    }
+
+    @available(iOS 11.0, tvOS 11.0, *)
+    public func replaceArrangedSubview(_ oldView: UIView, with newView: UIView) {
+        guard let indexToInsert = arrangedSubviews.firstIndex(of: oldView) else { return }
+
+        let spacingNext = customSpacing(after: oldView)
+        removeArrangedSubview(oldView)
+        insertArrangedSubview(newView, at: indexToInsert)
+        setCustomSpacing(spacingNext, after: newView)
+    }
+
+    private func getInsertionPosition(insertingView: UIView, exisitingView: UIView) -> Int? {
+        guard checkIfInsertable(insertingView: insertingView, existingView: exisitingView) else { return nil }
+
+        removeArrangedSubview(insertingView)
+        return arrangedSubviews.firstIndex(of: exisitingView)!
+    }
+
+    private func checkIfInsertable(insertingView: UIView, existingView: UIView) -> Bool {
+        return insertingView !== existingView && arrangedSubviewsSet.contains(existingView)
+    }
+
+    private var arrangedSubviewsSet: Set<UIView> {
+        return Set(arrangedSubviews)
+    }
 }
